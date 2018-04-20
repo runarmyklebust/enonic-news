de8443c5474693616a4aba1f262dafc38dc51d7c
Christian Westgaard <cwe@enonic.com>
2018-04-20 12:42:29 +0200

    responsive multicolor

--- src/main/resources/site/parts/grid/grid.js
+++ src/main/resources/site/parts/grid/grid.js
@@ -1,18 +1,49 @@
 var R = require('/lib/render-js/class.js');
 var P = require('/lib/xp/portal');
 
+var LORUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 var DATA = [{
-  displayName: 'Juhu1'
+  displayName: LORUM
 }, {
-  displayName: 'Juhu2'
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
+}, {
+  displayName: LORUM
 }];
 
 var LI = R.li(
   R.article([
-    R.h3()
+    //R.h3(),
+    R.p()
   ])
 );
 R.build(LI);
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 exports.get = function(request) {
   var partComponent = P.getComponent();
@@ -22,20 +53,34 @@ exports.get = function(request) {
       dataPortalComponentType: 'part',
       _s: {
         listStyle: 'none',
+        padding: '0',
         //alignContent: layoutComponent.config.alignContent,
         //alignItems: layoutComponent.config.alignItems,
         display: 'grid',
         gridColumnGap: '30px', //layoutComponent.config.gridColumnGap,
         //gridRowGap: layoutComponent.config.gridRowGap,
-        gridTemplateColumns: '1fr 1fr',//layoutComponent.config.gridTemplateColumns,
+        gridTemplateColumns: '1fr',//layoutComponent.config.gridTemplateColumns,
         //gridTemplateRows: layoutComponent.config.gridTemplateRows,
         //justifyContent: layoutComponent.config.justifyContent,
         //justifyItems: layoutComponent.config.justifyItems
+      },
+      _m: {
+        'minWidth375': {
+          gridTemplateColumns: '1fr 1fr'
+        },
+        'minWidth768': {
+          gridTemplateColumns: '1fr 1fr 1fr'
+        },
+        'minWidth1024': {
+          gridTemplateColumns: '1fr 1fr 1fr 1fr'
+        }
       }
     },
     DATA.map(function(item) {
       var anItem = R.clone(LI);
-      R.access(anItem, 'article.h3').setContent(item.displayName);
+      R.access(anItem, 'article.p')
+        .setContent(item.displayName)
+        .setStyle({backgroundColor: getRandomColor()});
       return anItem;
     }) // DATA map
   ); // dom
