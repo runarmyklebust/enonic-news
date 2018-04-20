0afe85b98e97ac4e15b8c79bf1d678e344517da0
Espen Norderud <espen42@gmail.com>
2018-04-20 14:02:06 +0200

    The amazing side bar

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -1,4 +1,4 @@
-var mustache = require('/lib/xp/mustache');
+var thymeleaf = require('/lib/xp/thymeleaf');
 var portalLib = require('/lib/xp/portal');
 
 var view = resolve('./default.html');
@@ -9,18 +9,46 @@ function handleGet(req) {
 
 
     var content = portalLib.getContent();
+    var site = portalLib.getSite();
+
+    var sourceFilterLinks = site.x["com-enonic-app-enonicnews"].filterlink.filterlink || [];
+
+    log.info(JSON.stringify({sourceFilterLinks:sourceFilterLinks}, null, 2));
+
+    var filterlinks = [];
+    for (var fl in sourceFilterLinks) {
+        var f = sourceFilterLinks[fl];
+        log.info(JSON.stringify({f:f}, null, 2));
+
+        if (f && f.label && f.href) {
+
+            var params = (f.href && f.href!=="/") ? {filter: f.href.toLowerCase()} : undefined;
+            log.info(JSON.stringify({params:params}, null, 2));
+
+            filterlinks.push({
+                src: portalLib.pageUrl({
+                    path: "/enonic-news",
+                    params: params,
+                }),
+                text: f.label[0].toUpperCase() + f.label.substring(1).toLowerCase(),
+            });
+        }
+    }
+
+    var sidebarStyleUri = portalLib.assetUrl({path: "css/sidebar.css"})
 
     var params = {
-        assetsUri: portalLib.assetUrl({path: ""}),
-        svcUrl: portalLib.serviceUrl({service: 'Z'}).slice(0, -1),
-
         mainRegion: content.page.regions.main,
-
+        filterlinks: filterlinks,
+        heading: site.displayName,
     };
 
     return {
         contentType: 'text/html',
-        body: mustache.render(view, params)
+        body: thymeleaf.render(view, params),
+        pageContributions: {
+            headBegin: '<link href="' + sidebarStyleUri + '" rel=\"stylesheet\">',
+        }
     };
 
 };
